1134c87f44e71ec22de8aa2613e8a9968053a30e
Phawinee <42910426+Faiifu@users.noreply.github.com>
2019-03-17 21:26:31 +0700

    edit hint

--- testtest.js
+++ testtest.js
@@ -1,3 +1,20 @@
 
 var audio = new Audio();
 audio.src = 'sound/click.mp3'
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
---
+++ testtest2.js
@@ -0,0 +1,2 @@
+var audio = new Audio();
+audio.src = 'sound/click.mp3'
