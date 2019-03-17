d7215e6c25f52811598cc8886a4808370ed2f485
Phawinee <42910426+Faiifu@users.noreply.github.com>
2019-03-17 20:01:55 +0700

    .

--- javascript.js
+++ javascript.js
@@ -539,3 +539,20 @@ Tile.prototype.updatePosition = function (position) {
 if(self.won === false) {
   document.getElementById('winsound').play();
 }
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
